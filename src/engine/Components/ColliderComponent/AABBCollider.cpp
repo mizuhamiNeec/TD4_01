4918015d348f04a4675ddb7c973fe08011b951d4
MizuHami <laptop@MizuHami>
2025-10-30 16:09:34 +0900

    ✨ AABBColliderコンポーネントを追加し、AABBの衝突判定とInspector表示機能を実装しました。

---
+++ src/engine/Components/ColliderComponent/AABBCollider.cpp
@@ -0,0 +1,50 @@
+#include "AABBCollider.h"
+
+#include "engine/Debug/Debug.h"
+#include "engine/ImGui/ImGuiWidgets.h"
+
+AABBCollider::AABBCollider(const Unnamed::AABB& aabb, const Vec3 offset)
+	: mAABB(aabb), mOffset(offset) {
+}
+
+void AABBCollider::Update(float) {
+	Debug::DrawBox(
+		mOwner->GetTransform()->GetWorldPos() + mOffset,
+		Quaternion::identity,
+		mAABB.Size(),
+		Vec4::red
+	);
+}
+
+void AABBCollider::DrawInspectorImGui() {
+#ifdef _DEBUG
+	if (
+		ImGui::CollapsingHeader("AABBCollider", ImGuiTreeNodeFlags_DefaultOpen)
+	) {
+		ImGui::SeparatorText("AABB");
+		ImGuiWidgets::DragVec3("Min", mAABB.min, Vec3::zero, 0.1f, "%.2f");
+		ImGuiWidgets::DragVec3("Max", mAABB.max, Vec3::zero, 0.1f, "%.2f");
+		ImGui::SeparatorText("Offset");
+		ImGuiWidgets::DragVec3("Offset", mOffset, Vec3::zero, 0.1f, "%.2f");
+	}
+#endif
+}
+
+void AABBCollider::CheckCollision(const ColliderComponent* other) const {
+	(void)other;
+}
+
+Unnamed::AABB& AABBCollider::AABB() {
+	return mAABB;
+}
+
+Vec3& AABBCollider::Offset() {
+	return mOffset;
+}
+
+Unnamed::AABB AABBCollider::GetOffsetAABB() const {
+	return Unnamed::AABB(
+		mAABB.min + mOffset,
+		mAABB.max + mOffset
+	);
+}

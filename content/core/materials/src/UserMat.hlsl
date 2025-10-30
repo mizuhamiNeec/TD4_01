55a2d07d26ad08da2ff4efb3c27631034fc9dfa6
MizuHami <laptop@MizuHami>
2025-10-30 16:07:58 +0900

    ✨ UserMat.hlslのテクスチャサンプリングロジックを簡素化し、ベースカラーの計算を修正しました。

--- content/core/materials/src/UserMat.hlsl
+++ content/core/materials/src/UserMat.hlsl
@@ -7,21 +7,12 @@ void ShadeMaterial(in MatIn IN, out MatOut OUT) {
 	uint tw, th, mipCount;
 	gTex.GetDimensions(0, tw, th, mipCount);
 
-	float4 texColor;
-	if (gUseForcedMip != 0) {
-		uint targetMip = (mipCount - 1) ?
-			                 (uint)round(
-				                 saturate(gForceMipNorm) * (mipCount - 1)) :
-			                 0;
-		texColor = gTex.SampleLevel(gLinearWrap, uv, targetMip);
-	} else {
-		texColor = gTex.Sample(gLinearWrap, uv);
-	}
+	float4 texColor = gTex.Sample(gLinearWrap, uv);
 
-	// float3 tex    = texColor.rgb;
-	// float  rough  = gExtra[0].x;
-	// float3 emi    = gExtra[1].rgb;
-	// float3 lit    = lerp(tex * 0.6, tex, saturate(1 - gMetallic));
-	OUT.baseColor = /*lit **/ gBaseColor.rgb * IN.normal.rgb;
+	float3 tex = texColor.rgb;
+	// float  rough = gExtra[0].x;
+	// float3 emi   = gExtra[1].rgb;
+	float3 lit    = lerp(tex * 0.5, tex, saturate(1 - gMetallic));
+	OUT.baseColor = tex;
 	OUT.metallic  = gMetallic;
 }

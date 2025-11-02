c0b77571e362b9a2c0b45b7199ea450a89375124
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-11-03 00:19:41 +0900

    ✨ Material: サンプラーのComparisonFuncを修正

--- src/engine/ResourceSystem/Material/Material.cpp
+++ src/engine/ResourceSystem/Material/Material.cpp
@@ -511,13 +511,13 @@ ID3D12RootSignature* Material::GetOrCreateRootSignature(
 			samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
 			samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
 			samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			samplerDesc.MinLOD = 0.0f;
+			samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
 			samplerDesc.MipLODBias = 0;
 			samplerDesc.MaxAnisotropy = 16;
-			samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+			samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_ALWAYS;
 			samplerDesc.BorderColor =
 				D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
-			samplerDesc.MinLOD           = 0.0f;
-			samplerDesc.MaxLOD           = D3D12_FLOAT32_MAX;
 			samplerDesc.ShaderRegister   = 0;
 			samplerDesc.RegisterSpace    = 0;
 			samplerDesc.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;

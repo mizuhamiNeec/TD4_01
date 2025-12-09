4baa1734f2530758370040fbb9ce89766fa34adc
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2025-12-09 10:05:12 +0900

    ✨ PhysicsTypes: Hit構造体のposとnormalにデフォルト値を追加

--- src/engine/uphysics/PhysicsTypes.h
+++ src/engine/uphysics/PhysicsTypes.h
@@ -7,10 +7,10 @@
 namespace UPhysics {
 	// ヒット情報
 	struct Hit {
-		float    t     = FLT_MAX; // 0～1
-		float    depth = 0.0f;
-		Vec3     pos;
-		Vec3     normal;
+		float    t          = FLT_MAX; // 0～1
+		float    depth      = 0.0f;
+		Vec3     pos        = Vec3::zero;
+		Vec3     normal     = Vec3::zero;
 		uint32_t triIndex   = UINT_FAST32_MAX;
 		bool     startSolid = false; // 開始時に形状が重なっていたか
 		bool     allsolid   = false; // トレース全域で固体内だったか

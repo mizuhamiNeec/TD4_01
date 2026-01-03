12f6561b4623350ab0ec0e69d51225da95ccca8b
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-01-03 22:52:31 +0900

    ✨ Frustum: 新しいFrustum構造体と関連する関数を追加しました

---
+++ src/runtime/render/utils/Frustum.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "runtime/core/math/Math.h"
+
+namespace Unnamed {
+	struct Plane {
+		Vec3  n = Vec3::zero;
+		float d = 0.0f;
+	};
+
+	struct Frustum {
+		Plane planes[6];
+
+		static Frustum FromViewProjRowVec(const Mat4& viewProj);
+
+		[[nodiscard]] bool TestSphere(const Vec3& centerWS,
+		                              float       radiusWS) const;
+	};
+}

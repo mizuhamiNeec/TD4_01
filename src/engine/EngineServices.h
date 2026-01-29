85279beb8fa5fee26b82da5080b49cfbcc777ea6
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-01-30 08:41:54 +0900

    ✨: EngineServicesクラスを追加し、エンジンの取得処理を実装しました

---
+++ src/engine/EngineServices.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <engine/unnamed/subsystem/interface/ServiceLocator.h>
+
+namespace Unnamed {
+	class Engine;
+
+	class EngineServices {
+	public:
+		static Engine* Get() { return ::ServiceLocator::Get<Unnamed::Engine>(); }
+	};
+}

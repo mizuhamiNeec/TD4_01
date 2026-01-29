fd95cefba142967fd1f614229eb2dae10fe406af
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-01-30 07:54:10 +0900

    ✨: EngineConfigにウィンドウ設定を追加し、ウィンドウモードを定義

--- src/engine/EngineConfig.h
+++ src/engine/EngineConfig.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <string>
 
 namespace Unnamed {
 	enum class ENGINE_MODE {
@@ -6,7 +7,23 @@ namespace Unnamed {
 		GAME,
 	};
 
+	enum class WINDOW_MODE : uint8_t {
+		WINDOWED,
+		BORDERLESS,
+		FULLSCREEN,
+	};
+
 	struct EngineConfig {
 		ENGINE_MODE mode;
+
+		struct Window {
+			std::string title     = "Unnamed Engine";
+			int32_t     width     = 1280;
+			int32_t     height    = 720;
+			WINDOW_MODE mode      = WINDOW_MODE::WINDOWED;
+			bool        resizable = true;
+
+			bool dpiAware = true; // DPI認識を有効にするか?
+		} window;
 	};
 }

ecf0291bbb79ca7ef7446bc06da1eb00f4ca48dd
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-01-30 07:54:31 +0900

    🔧: main.cppのエンジン実行部分を簡素化し、不要なコードを削除

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
-#include <pch.h>
-
 #include <engine/Engine.h>
-
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int) {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // リークチェック
-	[[maybe_unused]] HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-
-	// エンジンの作成・実行
-	Unnamed::Engine engine;
-	engine.Run();
-
-	CoUninitialize();
-	return EXIT_SUCCESS;
+	Unnamed::Engine engine;	// エンジンの作成・実行
+	return engine.Run();
 }

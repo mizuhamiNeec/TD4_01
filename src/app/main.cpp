983ea4e9d8a4f46132f30aeb699bcb5bfe780ba2
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:21:48 +0900

    🚚: main.cppを移動。

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,5 +1,6 @@
 #include <engine/Engine.h>
+
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int) {
-	Unnamed::Engine engine;	// エンジンの作成・実行
+	Unnamed::Engine engine; // エンジンの作成・実行
 	return engine.Run();
 }

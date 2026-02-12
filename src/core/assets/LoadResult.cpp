eb7298158822ad6bc2fc5fb0096aec43e1bcda12
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:24:20 +0900

    ✨: LoadResult構造体を追加し、アセットのペイロードデータと依存関係を管理する機能を実装

---
+++ src/core/assets/LoadResult.cpp
@@ -0,0 +1,1 @@
+﻿#include "LoadResult.h"
---
+++ src/core/assets/LoadResult.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <variant>
+#include <vector>
+
+#include "AssetID.h"
+#include "FileStamp.h"
+
+#include "types/TextureAsset.h"
+
+namespace Unnamed {
+	//-------------------------------------------------------------------------
+	// variant: 複数の型の一つを突っ込めるもの。 スッゲェェエ
+	// monostate: 空の状態
+	//-------------------------------------------------------------------------
+
+	/// @brief アセットのペイロードデータ
+	using AssetPayload = std::variant<
+		std::monostate,
+		TextureAssetData
+	>;
+
+	struct LoadResult {
+		AssetPayload         payload;      // アセットのペイロードデータ
+		std::vector<AssetID> dependencies; // パースして見つかった依存
+		FileStamp            stamp;        // ファイルのタイムスタンプ
+		std::string          resolveName;  // 表示名
+	};
+}

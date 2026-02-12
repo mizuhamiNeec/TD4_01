4b9b2642ad271cd03ac91b2800c5b3acc3934ce3
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:24:05 +0900

    ✨: FileStamp構造体を追加し、ファイルの最終更新日時とサイズを管理する機能を実装

---
+++ src/core/assets/FileStamp.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <chrono>
+
+namespace Unnamed {
+	struct FileStamp {
+		std::chrono::system_clock::time_point lastWrite   = {};
+		uint64_t                              sizeInBytes = 0;
+	};
+}

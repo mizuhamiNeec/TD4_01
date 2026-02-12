ce7c502bb7146986cc26dc0a4d2c0e8df18ca867
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:42:49 +0900

    ✨: ConVarHelperクラスを追加し、UIの表示機能を実装しました

---
+++ src/engine/unnamed/subsystem/console/ConVarHelper.h
@@ -0,0 +1,141 @@
+﻿#pragma once
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "core/math/Math.h"
+
+namespace Unnamed {
+	class ConsoleSystem;
+
+	/// @brief コンソール変数ヘルパークラス
+	class ConVarHelper {
+	public:
+		explicit ConVarHelper(ConsoleSystem* consoleSystem) :
+			mConsoleSystem(
+				consoleSystem
+			) {}
+
+		/// @brief ConVarヘルパーUIを表示します
+		void Show(bool& showConVarHelper);
+
+	private:
+		/// @brief メニューバーを表示します
+		void ShowMenuBar();
+
+		/// @brief ページ選択を表示します
+		void ShowPageSelect();
+
+		/// @brief グリッドサイズコントロールを表示します
+		void ShowGridSizeControls();
+
+		/// @brief グリッドを表示します
+		void ShowGrid();
+
+
+		// 要素編集
+		/// @brief 要素編集ポップアップを表示します
+		void ShowElementEditPopup();
+		void LabelEdit();            // ラベル編集
+		void ButtonEdit();           // ボタン編集
+		void ExecutableButtonEdit(); // 実行可能ボタン編集
+
+
+		void ImportPage();
+		void ExportPage();
+
+		void RearrangeGridElements(uint32_t newWidth, uint32_t newHeight);
+
+		void ShowExecResultPopup();
+
+		ConsoleSystem* mConsoleSystem = nullptr;
+
+		static constexpr auto kBgColorDefault = Vec4(0.29f, 0.29f, 0.29f, 1.0f);
+		static constexpr auto kFgColorDefault = Vec4(0.71f, 0.71f, 0.72f, 1.0f);
+
+		/// @brief 空の構造体
+		struct Empty {};
+
+		/// @brief ラベルの構造体
+		/// @details 区切りや説明用のラベル
+		struct Label {
+			std::string label;                     // ラベル名
+			Vec4        bgColor = kBgColorDefault; // 背景色
+			Vec4        fgColor = kFgColorDefault; // 前景色
+			std::string description;               // 説明
+		};
+
+		/// @brief ボタンの構造体
+		/// @details ConVar/ConCommandを実行するボタン
+		struct Button {
+			std::string label;                     // ボタン名
+			std::string command;                   // 実行するコマンド
+			Vec4        bgColor = kBgColorDefault; // 背景色
+			Vec4        fgColor = kFgColorDefault; // 前景色
+			std::string description;               // 説明
+		};
+
+		/// @brief 実行可能ボタンの構造体
+		/// @details exe/bat/cmd/etc.を実行するボタン
+		struct ExecutableButton {
+			std::string label;                     // ラベル名
+			std::string command;                   // 実行するexe/bat/cmd/etc.
+			std::string arguments;                 // 引数
+			Vec4        bgColor = kBgColorDefault; // 背景色
+			Vec4        fgColor = kFgColorDefault; // 前景色
+			std::string description;               // 説明
+		};
+
+		/// @brief グリッド要素の構造体
+		struct GridElement {
+			std::variant<Empty, Label, Button, ExecutableButton> element;
+		};
+
+		/// @brief ページの構造体
+		struct Page {
+			std::string name; // ページ名
+
+			struct Grid {
+				uint32_t width;            // テーブルの列数
+				uint32_t height;           // テーブルの行数
+			}                        grid; // グリッドサイズ
+			std::vector<GridElement> elements;
+		};
+
+		// ページ (空で4ページ用意しておく)
+		std::vector<Page> mPages = {
+			{
+				.name = "User Page 1",
+				.grid{.width = 3, .height = 10},
+				.elements = {}
+			},
+			{
+				.name = "User Page 2",
+				.grid{.width = 3, .height = 10},
+				.elements = {}
+			},
+			{
+				.name = "User Page 3",
+				.grid{.width = 3, .height = 10},
+				.elements = {}
+			},
+			{
+				.name = "User Page 4",
+				.grid{.width = 3, .height = 10},
+				.elements = {}
+			}
+		}; // ページ
+
+		size_t   mEditingElementIndex = 0;
+		uint32_t mSelectedPageIndex   = 0;
+		bool     mShowElementPopup    = false;
+
+		bool        mElementEditInitialized = false;
+		GridElement mElementEditOriginal{};
+		GridElement mElementEditWorking{};
+
+		// 下書き
+		ExecutableButton mElementEditDraftExecutableButton{};
+		std::string      mElementEditDraftButtonCommand{};
+	};
+}

e8a75a9e766226dbeb7db5b529191d7bfcdf7bf1
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:23:59 +0900

    ✨: ASSET_TYPE列挙型を追加し、アセットタイプの文字列変換機能を実装

---
+++ src/core/assets/AssetType.cpp
@@ -0,0 +1,14 @@
+﻿#include "AssetType.h"
+
+std::string Unnamed::ToString(const ASSET_TYPE e) {
+	switch (e) {
+		case ASSET_TYPE::UNKNOWN: return "UNKNOWN";
+		case ASSET_TYPE::TEXTURE: return "TEXTURE";
+		case ASSET_TYPE::SHADER: return "SHADER";
+		case ASSET_TYPE::MATERIAL: return "MATERIAL";
+		case ASSET_TYPE::MESH: return "MESH";
+		case ASSET_TYPE::SOUND: return "SOUND";
+		case ASSET_TYPE::RAW_FILE: return "RAW_FILE";
+		default: return "unknown";
+	}
+}
---
+++ src/core/assets/AssetType.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <cstdint>
+#include <string>
+
+namespace Unnamed {
+	enum class ASSET_TYPE : uint8_t {
+		UNKNOWN  = 1 << 0, // 不明
+		TEXTURE  = 1 << 1, // テクスチャ
+		SHADER   = 1 << 2, // シェーダー
+		MATERIAL = 1 << 3, // マテリアル
+		MESH     = 1 << 4, // メッシュ
+		SOUND    = 1 << 5, // サウンド
+		RAW_FILE = 1 << 6, // 生ファイル
+	};
+
+	std::string ToString(ASSET_TYPE e);
+}

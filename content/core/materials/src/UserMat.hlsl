d8863baf8aa5f0d4d7ecfd06eb1202d83379707a
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:16:19 +0900

    🎨: UserMat.hlslのコードをリファクタリングし、可読性を向上させました

--- content/core/materials/src/UserMat.hlsl
+++ content/core/materials/src/UserMat.hlsl
@@ -12,10 +12,10 @@ void ShadeMaterial(in MatIn IN, out MatOut OUT) {
 	float3 tex = texColor.rgb;
 	// float  rough = gExtra[0].x;
 	// float3 emi   = gExtra[1].rgb;
-	float3 lit    = lerp(tex * 0.5, tex, saturate(1 - gMetallic));
-	
-	lit = lerp(lit, float3(0.25,0.25,0.75f), IN.normal.y);
-	
+	float3 lit = lerp(tex * 0.5, tex, saturate(1 - gMetallic));
+
+	lit = lerp(lit, float3(0.25, 0.25, 0.75f), IN.normal.y);
+
 	OUT.baseColor = lit;
 	OUT.metallic  = gMetallic;
 }

c1dc6407aa6e066b2e232f524e12880089f4c8b6
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:46:21 +0900

    ✨: RhiTypes.hにデバイスおよびスワップチェーンの設定構造体を追加しました

---
+++ src/engine/rhi/RhiTypes.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace Unnamed::Rhi {
+	struct DeviceDesc {
+		bool enableDebugLayer         = false; // デバッグレイヤーを有効にするか?
+		bool enableGpuBasedValidation = false; // バリデーションレイヤーを有効にするか?
+	};
+
+	struct SwapChainDesc {
+		uint32_t width       = 1280;  // デフォルト幅
+		uint32_t height      = 720;   // デフォルト高さ
+		uint32_t bufferCount = 2;     // ダブルバッファリング
+		bool     vSync       = false; // 垂直同期を行うか?
+	};
+
+	enum class TEXTURE_FORMAT : uint8_t {
+		R8G8B8A8_UNORM,   // 32-bit RGBA
+		R10G10B10A2_UNORM // 10-10-10-2 RGBA
+	};
+
+	struct ClearColor {
+		float r = 0.0f;
+		float g = 0.0f;
+		float b = 0.0f;
+		float a = 1.0f;
+	};
+}

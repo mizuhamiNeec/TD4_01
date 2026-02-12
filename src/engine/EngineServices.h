060259eabfd3f20da83f30fb1dd0f23edfe34edc
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:40:22 +0900

    🔧: EngineServicesクラスのGetメソッドを修正し、ServiceLocatorの使用を最適化しました

--- src/engine/EngineServices.h
+++ src/engine/EngineServices.h
@@ -7,6 +7,6 @@ namespace Unnamed {
 
 	class EngineServices {
 	public:
-		static Engine* Get() { return ::ServiceLocator::Get<Unnamed::Engine>(); }
+		static Engine* Get() { return ServiceLocator::Get<Engine>(); }
 	};
 }

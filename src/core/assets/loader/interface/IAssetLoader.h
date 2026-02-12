eb395df206165b4b6299bb5c1a2672f41b2f356d
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-12 10:22:46 +0900

    ✨: IAssetLoaderインターフェースを追加し、アセットの読み込み機能を定義しました

---
+++ src/core/assets/loader/interface/IAssetLoader.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "core/assets/AssetType.h"
+#include "core/assets/LoadResult.h"
+
+namespace Unnamed {
+	/// @brief アセットローダーインターフェース
+	class IAssetLoader {
+	public:
+		virtual      ~IAssetLoader() = default;
+		virtual bool CanLoad(
+			std::string_view path, ASSET_TYPE* outType
+		) const = 0;
+
+		virtual LoadResult Load(const std::string& path) = 0;
+	};
+}

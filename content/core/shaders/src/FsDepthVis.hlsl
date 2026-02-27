3fabe9c61f1ef87bc6051a132ca1365841cf4fdd
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 11:46:18 +0900

    ✨: 深度可視化用のピクセルシェーダー(FsDepthVis.hlsl)を新規追加しました

---
+++ content/core/shaders/src/FsDepthVis.hlsl
@@ -0,0 +1,13 @@
+﻿Texture2D<float> gDepth : register(t0);
+SamplerState     gSampler : register(s0);
+
+struct VsOut {
+	float4 pos : SV_POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+float4 PsMain(VsOut i) : SV_Target {
+	float d = gDepth.Sample(gSampler, i.uv);
+	float v = 1.0 - saturate(d);
+	return float4(v, v, v, 1.0f);
+}

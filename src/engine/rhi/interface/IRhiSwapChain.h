f3254ad4a45723a11df6645a1172bdb211c8ee0f
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 14:42:56 +0900

    ✨: IRhiSwapChainにテクスチャフォーマット取得メソッドGetFormatを追加

--- src/engine/rhi/interface/IRhiSwapChain.h
+++ src/engine/rhi/interface/IRhiSwapChain.h
@@ -1,5 +1,7 @@
-﻿#pragma once
+#pragma once
 #include <cstdint>
+
+#include <engine/rhi/RhiTypes.h>
 
 namespace Unnamed::Rhi {
 	class IRhiSwapChain {
@@ -11,6 +13,8 @@ namespace Unnamed::Rhi {
 		[[nodiscard]] virtual uint32_t GetBufferCount() const = 0;
 
 		[[nodiscard]] virtual uint32_t GetCurrentBackBufferIndex() const = 0;
+
+		[[nodiscard]] virtual TEXTURE_FORMAT GetFormat() const = 0;
 
 		virtual void Resize(uint32_t width, uint32_t height) = 0;
 

2bfbd224a0564fcb0ca28f8f5dc2d0f2e000682b
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 12:40:39 +0900

    ✨: AssetTypeに新しいアセット種別を追加し、文字列変換処理を更新しました

--- src/core/assets/AssetType.cpp
+++ src/core/assets/AssetType.cpp
@@ -1,14 +1,17 @@
-﻿#include "AssetType.h"
+#include "AssetType.h"
 
 std::string Unnamed::ToString(const ASSET_TYPE e) {
 	switch (e) {
 		case ASSET_TYPE::UNKNOWN: return "UNKNOWN";
 		case ASSET_TYPE::TEXTURE: return "TEXTURE";
-		case ASSET_TYPE::SHADER: return "SHADER";
+		case ASSET_TYPE::SHADER_SOURCE: return "SHADER_SOURCE";
 		case ASSET_TYPE::MATERIAL: return "MATERIAL";
 		case ASSET_TYPE::MESH: return "MESH";
 		case ASSET_TYPE::SOUND: return "SOUND";
 		case ASSET_TYPE::RAW_FILE: return "RAW_FILE";
+		case ASSET_TYPE::SHADER_PROGRAM: return "SHADER_PROGRAM";
+		case ASSET_TYPE::MATERIAL_INSTANCE: return "MATERIAL_INSTANCE";
+		case ASSET_TYPE::POST_FX_CHAIN: return "POST_FX_CHAIN";
 		default: return "unknown";
 	}
 }

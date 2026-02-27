f9be2f350fbb3ab6532ff961241854176468d8f5
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 12:53:04 +0900

    ♻️: D3D12の型定義をD3D12Types.hに分離し、依存を整理しました

--- src/engine/renderer/D3D12.h
+++ src/engine/renderer/D3D12.h
@@ -8,6 +8,8 @@
 
 #include <engine/Properties.h>
 
+#include "D3D12Types.h"
+
 #include "engine/platform/Window.h"
 
 class SrvManager;
@@ -15,34 +17,6 @@ class SrvManager;
 /// @brief D3Dリソースリークチェッカー
 struct D3DResourceLeakChecker {
 	~D3DResourceLeakChecker();
-};
-
-/// @brief バッファフォーマット
-struct RenderTargetTexture {
-	Microsoft::WRL::ComPtr<ID3D12Resource> rtv;
-	D3D12_CPU_DESCRIPTOR_HANDLE            rtvHandle;
-	D3D12_GPU_DESCRIPTOR_HANDLE            srvHandleGPU;
-	uint32_t                               srvIndex;
-};
-
-/// @brief 深度ステンシルテクスチャ構造体
-struct DepthStencilTexture {
-	Microsoft::WRL::ComPtr<ID3D12Resource> dsv;
-	D3D12_CPU_DESCRIPTOR_HANDLE            dsvHandle;
-	D3D12_GPU_DESCRIPTOR_HANDLE            srvHandleGPU;
-	uint32_t                               srvIndex;
-};
-
-/// @brief レンダーパスターゲット構造体
-struct RenderPassTargets {
-	D3D12_CPU_DESCRIPTOR_HANDLE* pRTVs; // 複数可
-	UINT                         numRTVs;
-	D3D12_CPU_DESCRIPTOR_HANDLE* pDSV; // nullptr でもOK
-	Vec4                         clearColor;
-	float                        clearDepth;
-	uint8_t                      clearStencil;
-	bool                         bClearColor; // クリアカラーするか?
-	bool                         bClearDepth; // 深度クリアするか?
 };
 
 /// @brief D3D12レンダラークラス

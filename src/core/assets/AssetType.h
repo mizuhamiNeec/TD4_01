a4df82b18b794452a298ae9b527ba6631a094cdb
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 12:42:31 +0900

    ✨: ASSET_TYPEに新しいアセット種別を追加し、型をuint16_tに拡張しました

--- src/core/assets/AssetType.h
+++ src/core/assets/AssetType.h
@@ -1,16 +1,19 @@
-﻿#pragma once
+#pragma once
 #include <cstdint>
 #include <string>
 
 namespace Unnamed {
-	enum class ASSET_TYPE : uint8_t {
-		UNKNOWN  = 1 << 0, // 不明
-		TEXTURE  = 1 << 1, // テクスチャ
-		SHADER   = 1 << 2, // シェーダー
-		MATERIAL = 1 << 3, // マテリアル
-		MESH     = 1 << 4, // メッシュ
-		SOUND    = 1 << 5, // サウンド
-		RAW_FILE = 1 << 6, // 生ファイル
+	enum class ASSET_TYPE : uint16_t {
+		UNKNOWN           = 1 << 0, // 不明
+		TEXTURE           = 1 << 1, // テクスチャ
+		SHADER_SOURCE     = 1 << 2, // シェーダー
+		MATERIAL          = 1 << 3, // マテリアル
+		MESH              = 1 << 4, // メッシュ
+		SOUND             = 1 << 5, // サウンド
+		RAW_FILE          = 1 << 6, // 生ファイル
+		SHADER_PROGRAM    = 1 << 7, // シェーダープログラム
+		MATERIAL_INSTANCE = 1 << 8, // マテリアルインスタンス
+		POST_FX_CHAIN     = 1 << 9, // ポストFXチェーン
 	};
 
 	std::string ToString(ASSET_TYPE e);

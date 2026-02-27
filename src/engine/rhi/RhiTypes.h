84f5149dc87bd31f3f969ed18e596602c33e2e28
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 14:37:37 +0900

    ♻️: SwapChainDesc構造体にバックバッファフォーマット指定機能を追加し、TEXTURE_FORMATの定義位置を整理しました

--- src/engine/rhi/RhiTypes.h
+++ src/engine/rhi/RhiTypes.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <cstdint>
 
 namespace Unnamed::Rhi {
@@ -7,16 +7,20 @@ namespace Unnamed::Rhi {
 		bool enableGpuBasedValidation = false; // バリデーションレイヤーを有効にするか?
 	};
 
-	struct SwapChainDesc {
-		uint32_t width       = 1280;  // デフォルト幅
-		uint32_t height      = 720;   // デフォルト高さ
-		uint32_t bufferCount = 2;     // ダブルバッファリング
-		bool     vSync       = false; // 垂直同期を行うか?
-	};
-
 	enum class TEXTURE_FORMAT : uint8_t {
 		R8G8B8A8_UNORM,   // 32-bit RGBA
 		R10G10B10A2_UNORM // 10-10-10-2 RGBA
+	};
+
+	struct SwapChainDesc {
+		uint32_t width       = 1280; // デフォルト幅
+		uint32_t height      = 720;  // デフォルト高さ
+		uint32_t bufferCount = 2;    // ダブルバッファリング
+
+		// バックバッファのフォーマット
+		TEXTURE_FORMAT format = TEXTURE_FORMAT::R8G8B8A8_UNORM;
+
+		bool vSync = false; // 垂直同期を行うか?
 	};
 
 	struct ClearColor {

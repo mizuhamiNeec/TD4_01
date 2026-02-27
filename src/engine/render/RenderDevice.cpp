41aee53963721284975824c99908b2c0f7056f2f
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-02-27 14:56:18 +0900

    📝: パイプラインキャッシュ無効化処理のコメントを整理・簡素化

--- src/engine/render/RenderDevice.cpp
+++ src/engine/render/RenderDevice.cpp
@@ -36,11 +36,7 @@ namespace Unnamed::Render {
 	void RenderDevice::OnResize(const uint32_t width, const uint32_t height) {
 		mRhiDevice.OnResize(width, height);
 
-		// リサイズに伴うパイプラインの再作成が必要な場合はここで行う
-		mPipelineCache.InvalidateAll(); // TODO: 最初は雑で
-		// Scene 描画解像度は URenderer 側の SceneRenderRequest で管理する。
-		// ここで Registry をスワップチェーンサイズに追従させると
-		// Editor Viewport がメインウィンドウサイズへ引きずられるため抑止する。
+		mPipelineCache.InvalidateAll();
 	}
 
 	RgResourceRegistry& RenderDevice::GetRegistry() { return mRegistry; }
@@ -51,7 +47,7 @@ namespace Unnamed::Render {
 				const auto& meta = mAssetManager.Meta(id);
 				if (meta.type == ASSET_TYPE::SHADER_SOURCE) {
 					mShaderLibrary.InvalidateByShaderSource(id);
-					mPipelineCache.InvalidateAll(); // TODO: 最初は雑で
+					mPipelineCache.InvalidateAll();
 				}
 			}
 		);

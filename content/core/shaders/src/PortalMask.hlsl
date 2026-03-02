de3d0f8ee97d2c2cd934558ca4bafd86cfcdb33c
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-02 10:21:14 +0900

    🔧: ポータルクリップ処理のための定数バッファを追加し、シェーダーの構造を整理しました

--- content/core/shaders/src/PortalMask.hlsl
+++ content/core/shaders/src/PortalMask.hlsl
@@ -1,27 +1,38 @@
-struct VSOut {
+cbuffer FrameCB : register(b0) {
+    float4x4 gView;
+    float4x4 gProj;
+    float4x4 gViewProj;
+    float3   gCameraPos;
+    float    gTime;
+    float4   gPortalClipPlane;
+    float    gPortalClipEnabled;
+    float3   gFramePadding;
+}
+
+cbuffer ObjectCB : register(b1) {
+    float4x4 gWorld;
+    float4x4 gWorldInvTranspose;
+    float4   gSkinningInfo;
+};
+
+struct VsIn {
+    float3 pos : POSITION;
+    float2 uv  : TEXCOORD0;
+};
+
+struct VsOut {
     float4 pos : SV_POSITION;
     float2 uv  : TEXCOORD0;
 };
 
-VSOut VsMain(uint vid : SV_VertexID) {
-    VSOut o;
-    if (vid == 0) {
-        o.pos = float4(-1.0, 1.0, 0.0, 1.0);
-        o.uv  = float2(0.0, 0.0);
-    } else if (vid == 1) {
-        o.pos = float4(3.0, 1.0, 0.0, 1.0);
-        o.uv  = float2(2.0, 0.0);
-    } else {
-        o.pos = float4(-1.0, -3.0, 0.0, 1.0);
-        o.uv  = float2(0.0, 2.0);
-    }
+VsOut VsMain(VsIn i) {
+    VsOut o;
+    float4 wp = mul(float4(i.pos, 1.0f), gWorld);
+    o.pos = mul(wp, gViewProj);
+    o.uv = i.uv;
     return o;
 }
 
-float4 PsMain(VSOut i) : SV_TARGET {
-    float2 uv = i.uv * 0.5f;
-    float2 c = uv - float2(0.5f, 0.5f);
-    float r2 = dot(c, c);
-    clip(0.16f - r2); // 半径0.4の円形マスク
-    return float4(0.0, 0.0, 0.0, 1.0);
+void PsMain(VsOut i) {
+    (void)i;
 }

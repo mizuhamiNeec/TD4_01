05fd87557bd9f41bab6d670770187f6d5d82b06d
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-02 10:30:04 +0900

    ✨: SpriteOverlayシェーダーの頂点シェーダーを追加

---
+++ content/core/shaders/src/sprite_overlay.vs.hlsl
@@ -0,0 +1,34 @@
+cbuffer FrameCB : register(b0) {
+	float4x4 gView;
+	float4x4 gProj;
+	float4x4 gViewProj;
+	float3   gCameraPos;
+	float    gTime;
+	float4   gPortalClipPlane;
+	float    gPortalClipEnabled;
+	float3   gFramePadding;
+}
+
+cbuffer ObjectCB : register(b1) {
+	float4x4 gWorld;
+	float4x4 gWorldInvTranspose;
+	float4   gSkinningInfo;
+}
+
+struct VsIn {
+	float3 pos : POSITION;
+	float2 uv  : TEXCOORD0;
+};
+
+struct VsOut {
+	float4 pos : SV_POSITION;
+	float2 uv  : TEXCOORD0;
+};
+
+VsOut VsMain(VsIn input) {
+	VsOut output;
+	const float4 worldPos = mul(float4(input.pos, 1.0f), gWorld);
+	output.pos = mul(worldPos, gViewProj);
+	output.uv = input.uv;
+	return output;
+}

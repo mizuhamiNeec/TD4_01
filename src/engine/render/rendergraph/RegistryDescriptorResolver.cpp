d3a745460631e19634efc0b8ebe5c8e8c7223283
MizuHami <laptop@MizuHami>
2026-03-06 16:08:00 +0900

    ♻️ RegistryDescriptorResolver.cppのコードを整理し、可読性を向上させました

--- src/engine/render/rendergraph/RegistryDescriptorResolver.cpp
+++ src/engine/render/rendergraph/RegistryDescriptorResolver.cpp
@@ -10,21 +10,31 @@ namespace Unnamed::Render {
 
 	D3D12_GPU_DESCRIPTOR_HANDLE RegistryDescriptorResolver::GetSrv(
 		const uint32_t textureId
-	) const { return mReg.GetSrv(textureId); }
+	) const {
+		return mReg.GetSrv(textureId);
+	}
 
 	D3D12_GPU_DESCRIPTOR_HANDLE RegistryDescriptorResolver::GetUav(
 		const uint32_t textureId
-	) const { return mReg.GetUav(textureId); }
+	) const {
+		return mReg.GetUav(textureId);
+	}
 
 	D3D12_CPU_DESCRIPTOR_HANDLE RegistryDescriptorResolver::GetRtvCpu(
 		const uint32_t textureId
-	) const { return mReg.GetRtvCpu(textureId); }
+	) const {
+		return mReg.GetRtvCpu(textureId);
+	}
 
 	D3D12_CPU_DESCRIPTOR_HANDLE RegistryDescriptorResolver::GetDsvCpu(
 		const uint32_t textureId
-	) const { return mReg.GetDsvCpu(textureId); }
+	) const {
+		return mReg.GetDsvCpu(textureId);
+	}
 
 	ID3D12Resource* RegistryDescriptorResolver::GetResource(
 		const uint32_t textureId
-	) const { return mReg.GetResource(textureId); }
+	) const {
+		return mReg.GetResource(textureId);
+	}
 }

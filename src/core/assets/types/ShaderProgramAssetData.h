deee490d25817e4f6e80c5fd79fe72f3b6c3ba1b
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:06:50 +0900

    ✨: シェーダープログラムアセットのデータ構造体とステージ情報を追加

--- src/core/assets/types/ShaderProgramAssetData.h
+++ src/core/assets/types/ShaderProgramAssetData.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 namespace Unnamed {
+	/// @brief シェーダープログラムのステージごとの情報を表す構造体
 	struct ShaderProgramStage {
 		std::string                                      sourcePath;
 		std::string                                      entry;
@@ -12,6 +13,7 @@ namespace Unnamed {
 		std::vector<std::pair<std::string, std::string>> defines;
 	};
 
+	/// @brief シェーダープログラムアセットのデータ構造体
 	struct ShaderProgramAssetData {
 		std::string                       name;
 		std::optional<ShaderProgramStage> vs;

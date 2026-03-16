3b7baaaed723f4f7dc6ce1c237dfc737239c0f71
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:08:14 +0900

    ✨: PathUtilクラスに相対パスを解決するメソッドを追加

---
+++ src/core/path/PathUtil.cpp
@@ -0,0 +1,19 @@
+﻿#include "PathUtil.h"
+
+#include "core/string/StrUtil.h"
+
+namespace Path {
+	std::string ResolveRelativePath(
+		const std::filesystem::path& baseDir, std::string path
+	) {
+		if (path.empty()) {
+			return path;
+		}
+
+		std::filesystem::path p(path);
+		if (p.is_relative()) {
+			p = baseDir / p;
+		}
+		return Unnamed::StrUtil::NormalizePath(p.lexically_normal().string());
+	}
+}
---
+++ src/core/path/PathUtil.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <filesystem>
+#include <string>
+
+namespace Path {
+	/// @brief ベースディレクトリからの相対パスを解決する。パスがすでに絶対パスの場合は、そのまま正規化して返す。
+	/// @param baseDir ベースディレクトリ
+	/// @param path 解決するパス
+	/// @return 解決されたパス
+	std::string ResolveRelativePath(
+		const std::filesystem::path& baseDir, std::string path
+	);
+}

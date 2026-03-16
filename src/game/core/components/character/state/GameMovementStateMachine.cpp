c2c16dd098ff1d0757905cc9fbd9752d687e82cc
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:18:12 +0900

    ✨: GameMovementStateMachineクラスの追加と移動状態管理機能の実装

---
+++ src/game/core/components/character/state/GameMovementStateMachine.cpp
@@ -0,0 +1,67 @@
+﻿#include "GameMovementStateMachine.h"
+
+#include "engine/unnamed/subsystem/console/Log.h"
+#include "engine/unnamed/subsystem/interface/ServiceLocator.h"
+
+namespace Unnamed {
+	GameMovementStateMachine::GameMovementStateMachine()  = default;
+	GameMovementStateMachine::~GameMovementStateMachine() = default;
+
+	void GameMovementStateMachine::Init() {
+		mConsole = ServiceLocator::Get<ConsoleSystem>();
+		if (!mConsole) {
+			Error(
+				"GMSM",
+				"コンソールシステムの取得に失敗しました。"
+			);
+		}
+	}
+
+	void GameMovementStateMachine::Tick(
+		MovementContext& context, const float deltaTime
+	) {
+		if (mCurrentState) {
+			mCurrentState->Tick(context, deltaTime);
+			if (!context.requestedState.empty()) {
+				ChangeState(context.requestedState);
+				context.requestedState.clear();
+			}
+		}
+	}
+
+	void GameMovementStateMachine::ChangeState(std::string stateName) {
+		if (
+			mCurrentState &&
+			mCurrentState->GetStateName() == stateName
+		) {
+			// 同じ状態に変更しようとした場合は何もしない
+			return;
+		}
+
+		// 状態名に対応する状態が存在するか確認
+		const auto it = mStates.find(stateName);
+
+		if (it != mStates.end()) {
+			// 現在の状態を終了
+			if (mCurrentState) {
+				mCurrentState->Exit();
+			}
+			// 新しい状態に切り替えて開始
+			mCurrentState = it->second;
+			mCurrentState->Enter(mConsole);
+		}
+	}
+
+	void GameMovementStateMachine::AddState(
+		const std::shared_ptr<IMovementState>& state
+	) {
+		if (state) {
+			mStates[state->GetStateName()] = state;
+		}
+	}
+
+	std::shared_ptr<IMovementState>
+	GameMovementStateMachine::GetCurrentState() const {
+		return mCurrentState;
+	}
+}
---
+++ src/game/core/components/character/state/GameMovementStateMachine.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "interface/IMovementState.h"
+
+namespace Unnamed {
+	class ConsoleSystem;
+	class BaseCharacterComponent;
+
+	class GameMovementStateMachine {
+	public:
+		GameMovementStateMachine();
+		~GameMovementStateMachine();
+
+		void Init();
+		void Tick(MovementContext& context, float deltaTime);
+		void ChangeState(std::string stateName);
+		void AddState(const std::shared_ptr<IMovementState>& state);
+
+		[[nodiscard]] std::shared_ptr<IMovementState> GetCurrentState() const;
+
+	private:
+		ConsoleSystem* mConsole = nullptr;
+		BaseCharacterComponent* mCharacterComponent = nullptr;
+
+		std::unordered_map<std::string, std::shared_ptr<IMovementState>>
+		mStates;
+		std::shared_ptr<IMovementState> mCurrentState = nullptr;
+	};
+}

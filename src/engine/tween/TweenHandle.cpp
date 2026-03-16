d43b3ce5150869e2f3b8690961049d5626c6318a
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:15:27 +0900

    ✨: TweenHandle.cppのコードを整理し、可読性を向上

--- src/engine/tween/TweenHandle.cpp
+++ src/engine/tween/TweenHandle.cpp
@@ -8,35 +8,47 @@ namespace Unnamed {
 		const std::weak_ptr<ITweenPlayable>& playable
 	) : mPlayable(playable) {}
 
-	bool TweenHandle::IsValid() const { return !mPlayable.expired(); }
+	bool TweenHandle::IsValid() const {
+		return !mPlayable.expired();
+	}
 
 	bool TweenHandle::IsPlaying() const {
 		const auto playable = mPlayable.lock();
-		if (!playable) { return false; }
+		if (!playable) {
+			return false;
+		}
 		return playable->IsPlaying();
 	}
 
 	bool TweenHandle::IsComplete() const {
 		const auto playable = mPlayable.lock();
-		if (!playable) { return false; }
+		if (!playable) {
+			return false;
+		}
 		return playable->IsComplete();
 	}
 
 	void TweenHandle::Pause() {
 		const auto playable = mPlayable.lock();
-		if (!playable) { return; }
+		if (!playable) {
+			return;
+		}
 		playable->Pause();
 	}
 
 	void TweenHandle::Resume() const {
 		const auto playable = mPlayable.lock();
-		if (!playable) { return; }
+		if (!playable) {
+			return;
+		}
 		playable->Resume();
 	}
 
 	void TweenHandle::Kill(const bool complete) const {
 		const auto playable = mPlayable.lock();
-		if (!playable) { return; }
+		if (!playable) {
+			return;
+		}
 		playable->Kill(complete);
 	}
 }

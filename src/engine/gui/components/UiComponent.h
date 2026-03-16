9cf6ed243616f8d7befb2bde20af47e5a5e27624
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:20:09 +0900

    ✨: UiComponentクラスを追加し、UIコンポーネントの基本機能を実装

---
+++ src/engine/gui/components/UiComponent.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string_view>
+#include <vector>
+
+#include "core/json/JsonReader.h"
+#include "core/json/JsonWriter.h"
+
+namespace Unnamed::Gui {
+	struct UiDrawCommand;
+	class UiWidget;
+
+	class UiComponent {
+	public:
+		virtual ~UiComponent() = default;
+
+		[[nodiscard]] virtual std::string_view GetTypeName() const = 0;
+
+		virtual void OnAttached(UiWidget& owner);
+		virtual void OnDetached(UiWidget& owner);
+		virtual void OnTick(UiWidget& owner, float deltaTime);
+		virtual void OnBeforeLayout(UiWidget& owner);
+		virtual void OnAfterLayout(UiWidget& owner);
+		virtual void BuildDrawCommands(
+			const UiWidget& owner, std::vector<UiDrawCommand>& out
+		) const;
+		virtual void OnMouseEnter(UiWidget& owner);
+		virtual void OnMouseLeave(UiWidget& owner);
+		virtual void OnMouseDown(UiWidget& owner);
+		virtual void OnMouseUp(UiWidget& owner);
+		virtual void OnClick(UiWidget& owner);
+
+		virtual void Serialize(JsonWriter& writer) const;
+		virtual void Deserialize(const JsonReader& reader);
+	};
+}

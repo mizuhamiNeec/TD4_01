9e9a31746b754ce363c8200c3c60b21e9b2b06c0
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:06:01 +0900

    ✨: MaterialAssetData.hにマテリアルドメイン、描画状態、アセットデータの構造体を追加

--- src/core/assets/types/MaterialAssetData.h
+++ src/core/assets/types/MaterialAssetData.h
@@ -7,11 +7,13 @@
 #include "core/math/Vec4.h"
 
 namespace Unnamed {
+	/// @brief マテリアルのドメインを表す列挙型
 	enum class MATERIAL_DOMAIN : uint8_t {
 		UNLIT           = 0,
 		PBR_METAL_ROUGH = 1,
 	};
 
+	/// @brief マテリアルの描画状態を表す構造体
 	struct MaterialRenderStateData {
 		bool depthEnable  = true;
 		bool depthWrite   = true;
@@ -23,6 +25,7 @@ namespace Unnamed {
 		uint8_t stencilWriteMask = 0xFF;
 	};
 
+	/// @brief マテリアルアセットのデータ構造体
 	struct MaterialAssetData {
 		std::string name;
 

1c238ddbfa021dfb6fdc5d99e49ddd822476bf88
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-16 14:18:33 +0900

    ✨: PlayerCharacterControllerクラスの追加とプレイヤー入力処理機能の実装

---
+++ src/game/core/components/controller/PlayerCharacterController.cpp
@@ -0,0 +1,120 @@
+﻿#include "PlayerCharacterController.h"
+
+#include "../character/base/BaseCharacterComponent.h"
+
+#include "core/ComponentRegistry.h"
+
+#include "engine/ImGui/ImGuiWidgets.h"
+#include "engine/unnamed/framework/components/TransformComponent.h"
+#include "engine/unnamed/subsystem/console/Log.h"
+#include "engine/unnamed/subsystem/input/InputSystem.h"
+#include "engine/unnamed/subsystem/interface/ServiceLocator.h"
+#include "engine/world/World.h"
+
+namespace Unnamed {
+	PlayerCharacterController::~PlayerCharacterController() {}
+
+	void PlayerCharacterController::OnAttached() {
+		BaseCharacterController::OnAttached();
+
+		if (auto* inputSystem = ServiceLocator::Get<InputSystem>()) {
+			mInput = inputSystem;
+		} else {
+			Error(
+				GetComponentName(),
+				"UInputSystemを取得できませんでした。BaseCharacterControllerは入力を処理できません。"
+			);
+		}
+	}
+
+	void PlayerCharacterController::PrePhysicsTick(float) {
+		// 各入力を詰め込む
+		MovementFrameInput input;
+		if (!mInput) {
+			return;
+		}
+
+		if (mInput->IsHeld("moveright")) {
+			input.moveAxis.x += 1.0f;
+		}
+		if (mInput->IsHeld("moveleft")) {
+			input.moveAxis.x -= 1.0f;
+		}
+		if (mInput->IsHeld("forward")) {
+			input.moveAxis.z += 1.0f;
+		}
+		if (mInput->IsHeld("back")) {
+			input.moveAxis.z -= 1.0f;
+		}
+		if (mInput->IsHeld("moveup")) {
+			input.moveAxis.y += 1.0f;
+		}
+		if (mInput->IsHeld("movedown")) {
+			input.moveAxis.y -= 1.0f;
+		}
+
+		if (mInput->IsHeld("jump")) {
+			input.jumpPressed = true;
+		}
+
+		if (mInput->IsHeld("duck")) {
+			input.crouchPressed = true;
+		}
+
+		if (mInput->IsHeld("sprint")) {
+			input.sprintPressed = true;
+		}
+
+		input.moveAxis.Normalize();
+
+		if (auto* world = GetWorld()) {
+			const auto& cameraMgr = world->GetCameraManager();
+
+			const auto info = cameraMgr.GetCurrentCameraInfo();
+			if (info.valid) {
+				input.right   = info.camera.view.Inverse().GetRight();
+				input.up      = info.camera.view.Inverse().GetUp();
+				input.forward = info.camera.view.Inverse().GetForward();
+			}
+		}
+
+		mDebugMoveFrameInput = input;
+
+		if (mTarget) {
+			mTarget->AddMovementFrameInput(input);
+		}
+	}
+
+	std::string_view PlayerCharacterController::GetStableName() const {
+		return "game.PlayerCharacterController";
+	}
+
+	std::string_view PlayerCharacterController::GetComponentName() const {
+		return "PlayerCharacterController";
+	}
+
+#ifdef _DEBUG
+	void PlayerCharacterController::DrawInspectorImGui() {
+		ImGui::BeginDisabled();
+		ImGui::TextUnformatted("PlayerInput");
+		ImGuiWidgets::DragVec3(
+			"Move Axis", mDebugMoveFrameInput.moveAxis, Vec3::zero, 0.1f, "%.3f"
+		);
+		ImGuiWidgets::DragVec3(
+			"Right", mDebugMoveFrameInput.right, Vec3::zero, 0.1f, "%.3f"
+		);
+		ImGuiWidgets::DragVec3(
+			"Up", mDebugMoveFrameInput.up, Vec3::zero, 0.1f, "%.3f"
+		);
+		ImGuiWidgets::DragVec3(
+			"Forward", mDebugMoveFrameInput.forward, Vec3::zero, 0.1f, "%.3f"
+		);
+		ImGui::Checkbox("Jump", &mDebugMoveFrameInput.jumpPressed);
+		ImGui::Checkbox("Sprint", &mDebugMoveFrameInput.sprintPressed);
+		ImGui::Checkbox("Crouch", &mDebugMoveFrameInput.crouchPressed);
+		ImGui::EndDisabled();
+	}
+#endif
+
+	REGISTER_COMPONENT(PlayerCharacterController);
+}
---
+++ src/game/core/components/controller/PlayerCharacterController.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "../character/base/BaseCharacterComponent.h"
+
+#include "base/BaseCharacterController.h"
+
+namespace Unnamed {
+	/// @brief プレイヤーがキャラクターを制御するためのコンポーネントです。
+	class PlayerCharacterController : public BaseCharacterController {
+	public:
+		~PlayerCharacterController() override;
+
+		// ---- BaseComponent ------------------------------------------------
+		void OnAttached() override;
+
+		void                           PrePhysicsTick(float deltaTime) override;
+		[[nodiscard]] std::string_view GetStableName() const override;
+		[[nodiscard]] std::string_view GetComponentName() const override;
+
+#ifdef _DEBUG
+		void DrawInspectorImGui() override;
+#endif
+
+	protected:
+		InputSystem*       mInput = nullptr;
+		MovementFrameInput mDebugMoveFrameInput;
+	};
+}

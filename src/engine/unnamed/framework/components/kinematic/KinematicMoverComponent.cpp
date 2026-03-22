26164030d2322c814d50ce84ea2de6a467cf3f66
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-23 07:08:29 +0900

    ✨: KinematicMoverComponentクラスを追加し、運動状態の管理機能を実装

---
+++ src/engine/unnamed/framework/components/kinematic/KinematicMoverComponent.cpp
@@ -0,0 +1,121 @@
+﻿#include "KinematicMoverComponent.h"
+
+#include "../TransformComponent.h"
+
+#include "../../entity/Entity.h"
+
+namespace Unnamed {
+	class TransformComponent;
+
+	const KinematicMoverState& KinematicMoverComponent::GetState() const {
+		return mState;
+	}
+
+	Vec3 KinematicMoverComponent::GetDeltaPosition() const {
+		return mState.deltaPosition;
+	}
+
+	Vec3 KinematicMoverComponent::GetLinearVelocity() const {
+		return mState.linearVelocity;
+	}
+
+	Vec3 KinematicMoverComponent::GetStepDelta(const float stepSeconds) const {
+		if (mState.wasTeleported || stepSeconds <= 0.0f ||
+		    mState.frameDeltaTime <= 0.0f) {
+			return Vec3::zero;
+		}
+
+		return mState.linearVelocity * stepSeconds;
+	}
+
+	bool KinematicMoverComponent::WasTeleported() const {
+		return mState.wasTeleported;
+	}
+
+	void KinematicMoverComponent::MarkTeleported() {
+		mState.wasTeleported = true;
+	}
+
+	void KinematicMoverComponent::OnAttached() {
+		BaseComponent::OnAttached();
+
+		auto* transform = mOwner ? mOwner->GetComponent<TransformComponent>() :
+			                  nullptr;
+		if (!transform) {
+			return;
+		}
+
+		mState.positionPrev = transform->Position();
+		mState.positionCurr = transform->Position();
+		mState.rotationPrev = transform->Rotation();
+		mState.rotationCurr = transform->Rotation();
+		mState.linearVelocity = Vec3::zero;
+		mState.deltaPosition  = Vec3::zero;
+		mState.frameDeltaTime = 0.0f;
+		mState.wasTeleported  = false;
+	}
+
+	void KinematicMoverComponent::PrePhysicsTick(float) {
+		auto* transform = mOwner->GetComponent<TransformComponent>();
+		if (!transform) {
+			return;
+		}
+
+		mState.positionPrev = mState.positionCurr;
+		mState.rotationPrev = mState.rotationCurr;
+
+		mState.positionCurr = transform->Position();
+		mState.rotationCurr = transform->Rotation();
+
+		mState.wasTeleported = false;
+	}
+
+	void KinematicMoverComponent::OnTick(float deltaTime) {
+		BaseComponent::OnTick(deltaTime);
+
+		auto* transform = mOwner->GetComponent<TransformComponent>();
+		if (!transform) {
+			return;
+		}
+
+		mState.positionCurr = transform->Position();
+		mState.rotationCurr = transform->Rotation();
+
+		mState.deltaPosition = mState.positionCurr - mState.positionPrev;
+		mState.frameDeltaTime = deltaTime;
+		if (deltaTime > 0.0f) {
+			mState.linearVelocity = mState.deltaPosition / deltaTime;
+		} else {
+			mState.linearVelocity = Vec3::zero;
+		}
+	}
+
+	void KinematicMoverComponent::PostPhysicsTick(float) {
+	}
+
+	std::string_view KinematicMoverComponent::GetStableName() const {
+		return "engine.KinematicMover";
+	}
+
+	std::string_view KinematicMoverComponent::GetComponentName() const {
+		return "KinematicMover";
+	}
+
+	uint32_t KinematicMoverComponent::GetIcon() const {
+		return BaseComponent::GetIcon();
+	}
+
+	void KinematicMoverComponent::DrawInspectorImGui() {
+		BaseComponent::DrawInspectorImGui();
+	}
+
+	void KinematicMoverComponent::Deserialize(const JsonReader& reader) {
+		reader;
+	}
+
+	void KinematicMoverComponent::Serialize(JsonWriter& writer) const {
+		writer;
+	}
+
+	REGISTER_COMPONENT(KinematicMoverComponent);
+}
---
+++ src/engine/unnamed/framework/components/kinematic/KinematicMoverComponent.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include "../base/BaseComponent.h"
+
+#include "core/ComponentRegistry.h"
+#include "core/math/Quaternion.h"
+#include "core/math/Vec3.h"
+
+namespace Unnamed {
+	struct KinematicMoverState {
+		Vec3 positionPrev = Vec3::zero;
+		Vec3 positionCurr = Vec3::zero;
+
+		Quaternion rotationPrev = Quaternion::identity;
+		Quaternion rotationCurr = Quaternion::identity;
+
+		Vec3 linearVelocity = Vec3::zero;
+		Vec3 deltaPosition  = Vec3::zero;
+
+		float frameDeltaTime = 0.0f;
+		bool  wasTeleported  = false;
+	};
+
+	class KinematicMoverComponent : public BaseComponent {
+	public:
+		// ---- KinematicMoverComponent ---------------------------------------
+		[[nodiscard]] const KinematicMoverState& GetState() const;
+
+		[[nodiscard]] Vec3 GetDeltaPosition() const;
+
+		[[nodiscard]] Vec3 GetLinearVelocity() const;
+
+		[[nodiscard]] Vec3 GetStepDelta(float stepSeconds) const;
+
+		[[nodiscard]] bool WasTeleported() const;
+
+		void MarkTeleported();
+
+		// ---- BaseComponent -------------------------------------------------
+		void OnAttached() override;
+
+		void PrePhysicsTick(float deltaTime) override;
+		void OnTick(float deltaTime) override;
+		void PostPhysicsTick(float deltaTime) override;
+
+		[[nodiscard]] TickGroup GetTickGroup() const override {
+			return TickGroup::KinematicSource;
+		}
+
+		[[nodiscard]] std::string_view GetStableName() const override;
+		[[nodiscard]] std::string_view GetComponentName() const override;
+
+		[[nodiscard]] uint32_t GetIcon() const override;
+
+#ifdef _DEBUG
+		void DrawInspectorImGui() override;
+#endif
+
+		void Deserialize(const JsonReader& reader) override;
+		void Serialize(JsonWriter& writer) const override;
+
+	private:
+		KinematicMoverState mState;
+	};
+}

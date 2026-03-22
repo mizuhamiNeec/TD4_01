866cb046151ad18742af6223abc519fce19b4d09
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-23 07:06:16 +0900

    🔧: TextureLoaderDirectXTex.cppからsRGB形式変換のコードを削除

--- src/core/assets/loader/TextureLoaderDirectXTex.cpp
+++ src/core/assets/loader/TextureLoaderDirectXTex.cpp
@@ -93,27 +93,6 @@ namespace Unnamed {
 			return r;
 		}
 
-		// sRGB形式に変換
-		if (meta.format != MakeSRGB(meta.format)) {
-			Msg(kChannel, "テクスチャをsRGB形式に変換します: {}", path);
-			ScratchImage converted;
-			hr = Convert(
-				img.GetImages(),
-				img.GetImageCount(),
-				meta,
-				MakeSRGB(meta.format),
-				TEX_FILTER_CUBIC,
-				0.0,
-				converted
-			);
-			if (FAILED(hr)) {
-				Error(kChannel, "テクスチャのsRGB変換に失敗しました: {}, そのまま使用します。", path);
-			} else {
-				img  = std::move(converted);
-				meta = img.GetMetadata();
-			}
-		}
-
 		// ミップマップがない場合は生成する
 		if (meta.mipLevels <= 1) {
 			Msg(kChannel, "ミップマップが存在しないため生成します。: {}", path);

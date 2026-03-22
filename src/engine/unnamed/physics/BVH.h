1533a082e2d6e3f4ccf86387437e031fec8915b0
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-23 07:09:02 +0900

    ✨: BVH構造体にコライダーの移動性を追加し、ワールド行列を管理する機能を実装

--- src/engine/unnamed/physics/BVH.h
+++ src/engine/unnamed/physics/BVH.h
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <vector>
 
+#include <core/math/Mat4.h>
 #include <engine/unnamed/physics/BVHBuilder.h>
 
 /// @brief BVH構造体
@@ -10,12 +11,18 @@ struct BVH {
 	std::vector<uint32_t>                   triIndices;
 };
 
+enum class ColliderMobility : uint8_t {
+	Static,
+	Dynamic
+};
+
 /// @brief 登録されたBVH構造体
 struct RegisteredBVH {
 	std::vector<Unnamed::Physics::FlatNode> nodes;
 	std::vector<uint32_t>                   triIndices;
+	std::vector<Unnamed::Triangle>          triangles;
 
-	size_t   triStart  = 0;
-	size_t   triCount  = 0;
-	uint64_t ownerGuid = 0;
+	uint64_t         ownerGuid = 0;
+	ColliderMobility mobility  = ColliderMobility::Static;
+	Mat4             world     = Mat4::identity;
 };

198919134d376fe93980b42f35ef84ba0da48151
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-03-25 02:21:04 +0900

    ✨: NoclipMoveクラスを追加し、移動状態管理を拡張

---
+++ src/game/core/components/character/state/NoclipMove.cpp
@@ -0,0 +1,29 @@
+﻿#include "NoclipMove.h"
+
+namespace Unnamed {
+	void NoclipMove::Enter(ConsoleSystem* console) {
+		console;
+	}
+
+	void NoclipMove::Tick(MovementContext& context, float deltaTime) {
+		context.isGrounded = false; // Noclip中は常に空中にいるとみなす
+
+		deltaTime;
+		
+		// Vec3  wishDir  = GetWishDir(context); // Noclipは水平・垂直両方の入力を考慮する
+		// float maxSpeed = mMaxSpeed->GetValue() * mNoclipSpeed->GetValue();
+		// float wishSpeed = 
+		//
+		// Accelerate(
+		// 	context.velocity, wishDir, maxSpeed,
+		// 	mNoclipAccelerate->GetValue(),
+		// 	deltaTime
+		// );
+	}
+
+	void NoclipMove::Exit() {}
+
+	std::string_view NoclipMove::GetStateName() {
+		return "NoclipMove";
+	}
+}
---
+++ src/game/core/components/character/state/NoclipMove.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "engine/unnamed/subsystem/console/concommand/ConVar.h"
+
+#include "interface/IMovementState.h"
+
+namespace Unnamed {
+	class NoclipMove : public IMovementState {
+	public:
+		void Enter(ConsoleSystem* console) override;
+		void Tick(MovementContext& context, float deltaTime) override;
+		void Exit() override;
+		std::string_view GetStateName() override;
+
+	protected:
+		ConVar<float>* mNoclipAccelerate = nullptr;
+		ConVar<float>* mNoclipSpeed      = nullptr;
+		ConVar<float>* mMaxSpeed         = nullptr;
+	};
+}

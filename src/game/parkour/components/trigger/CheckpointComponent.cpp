0d26c7e0f68cc7742c3df0a8275ffffbf99ac234
MizuHami <laptop@MizuHami>
2026-04-15 16:20:14 +0900

    ✨: CheckpointComponentにコースIDのデフォルト値設定とデバッグ用の入力フィールドを追加

--- src/game/parkour/components/trigger/CheckpointComponent.cpp
+++ src/game/parkour/components/trigger/CheckpointComponent.cpp
@@ -2,6 +2,9 @@
 
 #ifdef _DEBUG
 #include <imgui.h>
+#include <array>
+#include <algorithm>
+#include <cstring>
 #endif
 
 #include "core/ComponentRegistry.h"
@@ -17,6 +20,10 @@ namespace Unnamed {
 		}
 
 		mRespawnPosition = reader["respawnPosition"].GetVec3(mRespawnPosition);
+		mCourseId        = reader["courseId"].GetString(mCourseId);
+		if (mCourseId.empty()) {
+			mCourseId = "default";
+		}
 	}
 
 	void CheckpointComponent::Serialize(JsonWriter& writer) const {
@@ -24,6 +31,8 @@ namespace Unnamed {
 		writer.Key("index");
 		writer.Write(mIndex);
 		writer.WriteVec3("respawnPosition", mRespawnPosition);
+		writer.Key("courseId");
+		writer.Write(mCourseId);
 	}
 
 #ifdef _DEBUG
@@ -31,6 +40,24 @@ namespace Unnamed {
 		DrawVolumeInspectorImGui();
 		ImGui::InputInt("Index", &mIndex);
 		ImGui::DragFloat3("Respawn Position", &mRespawnPosition.x, 1.0f);
+		std::array<char, 64> courseIdBuffer = {};
+		const size_t copyLen                = std::min(
+			mCourseId.size(),
+			courseIdBuffer.size() - 1
+		);
+		if (copyLen > 0) {
+			std::memcpy(courseIdBuffer.data(), mCourseId.data(), copyLen);
+		}
+		if (ImGui::InputText(
+			"Course Id",
+			courseIdBuffer.data(),
+			courseIdBuffer.size()
+		)) {
+			mCourseId = courseIdBuffer.data();
+		}
+		if (mCourseId.empty()) {
+			mCourseId = "default";
+		}
 	}
 #endif
 
--- src/game/parkour/components/trigger/CheckpointComponent.h
+++ src/game/parkour/components/trigger/CheckpointComponent.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "TriggerVolumeComponentBase.h"
 
 namespace Unnamed {
@@ -31,8 +33,14 @@ namespace Unnamed {
 			return mRespawnPosition;
 		}
 
+		/// @brief 所属コースIDを返します。
+		[[nodiscard]] const std::string& GetCourseId() const noexcept {
+			return mCourseId;
+		}
+
 	private:
 		int32_t mIndex           = 0;
 		Vec3    mRespawnPosition = Vec3::zero;
+		std::string mCourseId    = "default";
 	};
 }

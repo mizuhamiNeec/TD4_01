32181dad87a8abcf72e14c8b23a273e8acf3a846
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-04-24 10:24:34 +0900

    ✨: MeshSubMeshRange構造体を追加し、MeshBufferにサブメッシュのベクターを定義しました

--- src/engine/rhi/Buffer.h
+++ src/engine/rhi/Buffer.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <cstdint>
 #include <d3d12.h>
+#include <vector>
 
 #include <wrl/client.h>
 
 #include "engine/unnamed/primitive/Primitives.h"
 
 namespace Unnamed::Render {
+	struct MeshSubMeshRange {
+		uint32_t indexStart    = 0;
+		uint32_t indexCount    = 0;
+		uint32_t materialIndex = 0;
+	};
+
 	struct MeshBuffer {
 		Microsoft::WRL::ComPtr<ID3D12Resource> vb;
 		Microsoft::WRL::ComPtr<ID3D12Resource> ib;
@@ -15,6 +22,7 @@ namespace Unnamed::Render {
 		D3D12_INDEX_BUFFER_VIEW  ibv = {};
 
 		uint32_t indexCount = 0;
+		std::vector<MeshSubMeshRange> submeshes = {};
 
 		AABB localAABB;
 	};

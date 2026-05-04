1176b5e583c1cfb611e6db6fc08f61d3c8058843
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-05-04 18:07:20 +0900

    ✨: EditorGuiScriptPanelクラスを追加し、Luaスクリプトの読み込みと描画機能を実装

---
+++ src/engine/editor/EditorGuiScriptPanel.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <core/assets/AssetManager.h>
+
+#include "engine/unnamed/subsystem/EditorLuaSystem/EditorLuaSystem.h"
+
+namespace Unnamed {
+	/// @brief luaでエディタを拡張するためのGUIパネル
+	class EditorGuiScriptPanel {
+	public:
+		void Initialize(AssetManager* assetManager, EditorLuaSystem* luaSystem);
+		void SetScriptPath(std::string path);
+		void Reload();
+		void Draw();
+
+	private:
+		AssetManager*    mAssetManager    = nullptr;
+		EditorLuaSystem* mEditorLuaSystem = nullptr;
+
+		std::string mScriptPath;
+		AssetID     mAssetID = 0;
+
+		bool mLoaded   = false;
+		bool mHasError = false;
+
+		std::string mLastError;
+	};
+}

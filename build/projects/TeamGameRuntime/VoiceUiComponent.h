4d4a387b7b0a307e50992c920d1297ba9f71986f
katajima <k023g1078@g.neec.ac.jp>
2026-05-29 12:49:19 +0900

    一旦

---
+++ build/projects/TeamGameRuntime/VoiceUiComponent.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "engine/unnamed/framework/components/base/BaseComponent.h"
+#include <core/io/json/JsonReader.h>
+#include <core/io/json/JsonWriter.h>
+#include <./core/math/Vec2.h>
+#include <./core/math/Vec3.h>
+
+namespace MyGame {
+	class VoiceUiComponent : public Unnamed::BaseComponent {
+
+
+
+	};
+}

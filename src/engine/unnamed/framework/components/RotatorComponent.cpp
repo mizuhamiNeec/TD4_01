50f357d508b667c6572101060c92c15817f14503
MizuHami <laptop@MizuHami>
2026-06-01 17:15:34 +0900

    ✨: RotatorComponentを追加し、回転速度の設定機能を実装しました。また、タイトル画面のJSONデータを更新しました

---
+++ src/engine/unnamed/framework/components/RotatorComponent.cpp
@@ -0,0 +1,70 @@
+﻿#include "RotatorComponent.h"
+
+#include <engine/ImGui/ImGuiWidgets.h>
+
+#include "TransformComponent.h"
+
+#include "../entity/Entity.h"
+
+#include "core/ComponentRegistry.h"
+#include "core/io/json/JsonReader.h"
+#include "core/io/json/JsonWriter.h"
+
+#include "engine/unnamed/subsystem/console/Log.h"
+
+#include "core/ComponentRegistry.h"
+
+namespace Unnamed {
+	RotatorComponent::~RotatorComponent() = default;
+
+	void RotatorComponent::OnAttached() {
+		mTransform = GetOwner()->GetComponent<TransformComponent>();
+	}
+
+	void RotatorComponent::OnTick(const float deltaTime) {
+		if (!mTransform) {
+			Error(GetComponentName(), "TransformComponent is null.");
+			return;
+		}
+
+		Quaternion current = mTransform->GetRotation();
+
+		current = current * Quaternion::EulerDegrees(mRotationSpeed * deltaTime);
+		mTransform->SetRotation(current);
+	}
+	
+	BaseComponent::TICK_GROUP RotatorComponent::GetTickGroup() const {
+		return BaseComponent::GetTickGroup();
+	}
+
+	std::string_view RotatorComponent::GetStableName() const {
+		return "engine.Rotator";
+	}
+
+	std::string_view RotatorComponent::GetComponentName() const {
+		return "Rotator";
+	}
+
+	void RotatorComponent::Deserialize(const JsonReader& reader) {		
+		mRotationSpeed   = reader["rotationSpeed"].GetVec3(mRotationSpeed);
+	}
+
+	void RotatorComponent::Serialize(JsonWriter& writer) const {
+		writer.Key("rotationSpeed");
+		writer.BeginArray();
+		writer.Write(mRotationSpeed.x);
+		writer.Write(mRotationSpeed.y);
+		writer.Write(mRotationSpeed.z);
+		writer.EndArray();
+	}
+
+	uint32_t RotatorComponent::GetIcon() const {
+		return BaseComponent::GetIcon();
+	}
+
+	void RotatorComponent::DrawInspectorImGui() {
+		ImGuiWidgets::DragVec3("RotationSpeed", mRotationSpeed,Vec3::zero,0.25f,"%.2f [deg/s]");
+	}
+	
+	REGISTER_COMPONENT(RotatorComponent);
+}
---
+++ src/engine/unnamed/framework/components/RotatorComponent.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "base/BaseComponent.h"
+
+#include "core/math/Vec3.h"
+
+namespace Unnamed {
+	class TransformComponent;
+
+	class RotatorComponent : public BaseComponent {
+	public:
+		~RotatorComponent() override;
+		
+		//---------------------------------------------------------------------
+		// BaseComponent↓ 
+		//---------------------------------------------------------------------
+		void                           OnAttached() override;
+		void                           OnTick(float deltaTime) override;
+		
+		[[nodiscard]] TICK_GROUP       GetTickGroup() const override;
+		
+		[[nodiscard]] std::string_view GetStableName() const override;
+		[[nodiscard]] std::string_view GetComponentName() const override;
+		
+		void                           Deserialize(const JsonReader& reader) override;
+		void                           Serialize(JsonWriter& writer) const override;
+		
+		[[nodiscard]] uint32_t         GetIcon() const override;
+		
+#ifdef _DEBUG
+		void                           DrawInspectorImGui() override;
+#endif
+		
+	private:
+		TransformComponent* mTransform = nullptr;
+		
+		Vec3 mRotationSpeed = Vec3::zero; // [度/秒]
+	};
+}

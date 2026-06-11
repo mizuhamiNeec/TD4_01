b849e037bc68302f946255b167b1dd2c2116824f
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-06-12 01:40:05 +0900

    🔧: GamePauseコンポーネントを登録する処理を追加

--- projects/TeamGame/runtime/game/team/runtime/TeamGameComponentRegistration.cpp
+++ projects/TeamGame/runtime/game/team/runtime/TeamGameComponentRegistration.cpp
@@ -15,6 +15,7 @@
 #include "GolfBallStartPosComponent.h"
 #include "GolfBallEndPosComponent.h"
 #include "FakeShadowComponent.h"
+#include "GamePause.h"
 #include "TrashObjMoverComponent.h"
 #include "TrashObjSpawnerComponent.h"
 #include "PlayerHoleComponent.h"
@@ -31,9 +32,10 @@ namespace Unnamed {
 	namespace {
 		template <typename T>
 		void RegisterComponentIfMissing(
-			ComponentRegistry &componentRegistry,
+			ComponentRegistry&     componentRegistry,
 			const std::string_view stableName,
-			const std::string_view displayName) {
+			const std::string_view displayName
+		) {
 			if (componentRegistry.IsRegistered(stableName)) {
 				return;
 			}
@@ -43,69 +45,99 @@ namespace Unnamed {
 				[]() -> std::unique_ptr<BaseComponent> {
 					return std::make_unique<T>();
 				},
-				displayName);
+				displayName
+			);
 			if (!registered) {
 				Warning(
 					"TeamGameRuntime",
 					"Failed to register game component '{}'.",
-					stableName);
+					stableName
+				);
 			}
 		}
 	}
 
-	void RegisterTeamGameComponents(ComponentRegistry &componentRegistry) {
+	void RegisterTeamGameComponents(ComponentRegistry& componentRegistry) {
 		RegisterEventPresentationComponent(componentRegistry);
 		RegisterComponentIfMissing<Unnamed::AudioFxControllerComponent>(
-			componentRegistry, "game.AudioFxController", "AudioFxController");
+			componentRegistry, "game.AudioFxController", "AudioFxController"
+		);
 		RegisterComponentIfMissing<Unnamed::CameraFxControllerComponent>(
-			componentRegistry, "game.CameraFxController", "CameraFxController");
+			componentRegistry, "game.CameraFxController", "CameraFxController"
+		);
 		RegisterComponentIfMissing<MyGame::MicComponent>(
-			componentRegistry, "mygame.MicComponent", "Mic Component");
+			componentRegistry, "mygame.MicComponent", "Mic Component"
+		);
 		RegisterComponentIfMissing<MyGame::PlayerFollowCameraComponent>(
 			componentRegistry, "mygame.PlayerFollowCameraComponent",
-			"Player Follow Camera Component");
+			"Player Follow Camera Component"
+		);
 		RegisterComponentIfMissing<MyGame::PlayerMoveComponent>(
-			componentRegistry, "mygame.PlayerMoveComponent", "Player Move Component");
+			componentRegistry, "mygame.PlayerMoveComponent",
+			"Player Move Component"
+		);
 		RegisterComponentIfMissing<MyGame::PlayerControlComponent>(
-			componentRegistry, "mygame.PlayerControlComponent", "Player Control Component");
+			componentRegistry, "mygame.PlayerControlComponent",
+			"Player Control Component"
+		);
 		RegisterComponentIfMissing<MyGame::GolfBallComponent>(
-			componentRegistry, "mygame.GolfBall", "Golf Ball");
+			componentRegistry, "mygame.GolfBall", "Golf Ball"
+		);
 		RegisterComponentIfMissing<MyGame::GolfBallLaunchCountdownComponent>(
 			componentRegistry, "mygame.GolfBallLaunchCountdownComponent",
-			"Golf Ball Launch Countdown Component");
+			"Golf Ball Launch Countdown Component"
+		);
 		RegisterComponentIfMissing<MyGame::GolfBallStartPosComponent>(
-			componentRegistry, "mygame.GolfBallStartPos", "Golf Ball Start Position");
+			componentRegistry, "mygame.GolfBallStartPos",
+			"Golf Ball Start Position"
+		);
 		RegisterComponentIfMissing<MyGame::GolfBallEndPosComponent>(
-			componentRegistry, "mygame.GolfBallEndPos", "Golf Ball End Position");
+			componentRegistry, "mygame.GolfBallEndPos", "Golf Ball End Position"
+		);
 		RegisterComponentIfMissing<MyGame::FakeShadowComponent>(
-			componentRegistry, "mygame.FakeShadow", "Fake Shadow");
+			componentRegistry, "mygame.FakeShadow", "Fake Shadow"
+		);
 		RegisterComponentIfMissing<MyGame::TrashObjMoverComponent>(
-			componentRegistry, "mygame.TrashObjMoverComponent", "Trash Obj");
+			componentRegistry, "mygame.TrashObjMoverComponent", "Trash Obj"
+		);
 		RegisterComponentIfMissing<MyGame::TrashObjSpawnerComponent>(
-			componentRegistry, "mygame.TrashObjSpawnerComponent", "ゴミ自動生成");
+			componentRegistry, "mygame.TrashObjSpawnerComponent", "ゴミ自動生成"
+		);
 		RegisterComponentIfMissing<MyGame::PlayerHoleComponent>(
-			componentRegistry, "mygame.PlayerHoleComponent", "Player Hole Component");
+			componentRegistry, "mygame.PlayerHoleComponent",
+			"Player Hole Component"
+		);
 		RegisterComponentIfMissing<MyGame::VoiceShockWaveComponent>(
-			componentRegistry, "mygame.VoiceShockWave", "Voice Shock Wave");
+			componentRegistry, "mygame.VoiceShockWave", "Voice Shock Wave"
+		);
 		RegisterComponentIfMissing<MyGame::VoiceUiComponent>(
-			componentRegistry, "mygame.VoiceUiComponent", "Voice Ui Component");
+			componentRegistry, "mygame.VoiceUiComponent", "Voice Ui Component"
+		);
 		RegisterComponentIfMissing<MyGame::GolfBallUiComponent>(
-			componentRegistry, "mygame.GolfBallUiComponent", "Golf Ball Ui Component");
+			componentRegistry, "mygame.GolfBallUiComponent",
+			"Golf Ball Ui Component"
+		);
 		RegisterComponentIfMissing<MyGame::Player2BallRangeUIComponent>(
 			componentRegistry, "mygame.Player2BallRangeUIComponent",
-			"Player To Ball Range UI Component");
+			"Player To Ball Range UI Component"
+		);
 		RegisterComponentIfMissing<MyGame::ResultScoreUiComponent>(
 			componentRegistry, "mygame.ResultScoreUiComponent",
-			"Result Score Ui Component");
+			"Result Score Ui Component"
+		);
 		RegisterComponentIfMissing<MyGame::GameRuleSystemComponent>(
 			componentRegistry, "mygame.GameRuleSystemComponent",
-			"Game Rule System Component");
+			"Game Rule System Component"
+		);
 		RegisterComponentIfMissing<MyGame::GameScoreComponent>(
 			componentRegistry, "mygame.GameScoreComponent",
-			"Game Score Component");
+			"Game Score Component"
+		);
 		RegisterComponentIfMissing<TitleController>(
 			componentRegistry, "mygame.TitleController", "Title Controller"
 		);
-
+		RegisterComponentIfMissing<GamePause>(
+			componentRegistry, "mygame.GamePause", "GamePause"
+		);
 	}
 }

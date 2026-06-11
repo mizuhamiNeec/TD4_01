57e5fa08848b2ceeb0055613fc473f5e5511d62c
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2026-06-12 01:40:12 +0900

    🔧: GamePauseコンポーネントを追加し、シリアライズ・デシリアライズ機能を実装

---
+++ projects/TeamGame/runtime/game/team/runtime/GamePause.cpp
@@ -0,0 +1,74 @@
+﻿#include "GamePause.h"
+
+#include <core/io/json/JsonReader.h>
+#include <core/io/json/JsonWriter.h>
+
+#include <engine/unnamed/subsystem/console/Log.h>
+#include <engine/unnamed/subsystem/input/InputSystem.h>
+
+namespace Unnamed {
+	std::string_view GamePause::GetStableName() const {
+		return "mygame.GamePause";
+	}
+
+	std::string_view GamePause::GetComponentName() const {
+		return "GamePause";
+	}
+
+	void GamePause::Deserialize(const JsonReader& reader) {
+		mPauseCanvasGuid =
+			reader.ReadUint64("pauseCanvasGuid").value_or(
+				mPauseCanvasGuid
+			);
+
+		mSettingsCanvasGuid =
+			reader.ReadUint64("settingsCanvasGuid").value_or(
+				mSettingsCanvasGuid
+			);
+	}
+
+	void GamePause::Serialize(JsonWriter& writer) const {
+		writer.Key("pauseCanvasGuid");
+		writer.Write(mPauseCanvasGuid);
+		writer.Key("settingsCanvasGuid");
+		writer.Write(mSettingsCanvasGuid);
+	}
+
+	void GamePause::OnAttached() {
+		mConsole   = GetConsoleSystem();
+		mInput     = GetInputSystem();
+		mTimeScale = mConsole ?
+			             mConsole->GetConVarAs<ConVar<float>>(
+				             "host_timescale"
+			             ) :
+			             nullptr;
+	}
+
+	// OnFrameInputTickはゲーム内時間に影響されない
+	void GamePause::OnFrameInputTick(float frameDeltaTime) {
+		// 入力とコンソールが取得できないのであれば何もしない
+		if (!mInput || !mConsole) {
+			return;
+		}
+
+		// 入力を取る
+		if (mInput->IsPressed("togglepause")) {
+			mIsPaused = !mIsPaused;
+		}
+
+		// 時間を止めるンゴ
+		if (mIsPaused) {
+			mTimeScale->SetValue(0.0f);
+		} else {
+			mTimeScale->SetValue(1.0f);
+		}
+	}
+
+	void GamePause::DrawInspectorImGui() {
+		BaseComponent::DrawInspectorImGui();
+	}
+
+	BaseComponent::TICK_GROUP GamePause::GetTickGroup() const {
+		return TICK_GROUP::EARLY;
+	}
+}
---
+++ projects/TeamGame/runtime/game/team/runtime/GamePause.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <engine/unnamed/framework/components/base/BaseComponent.h>
+#include <engine/unnamed/subsystem/console/concommand/ConVar.h>
+
+namespace Unnamed {
+	class GamePause : public BaseComponent {
+	public:
+		[[nodiscard]] std::string_view GetStableName() const override;
+		[[nodiscard]] std::string_view GetComponentName() const override;
+		void Deserialize(const JsonReader& reader) override;
+		void Serialize(JsonWriter& writer) const override;
+		void OnAttached() override;
+		void OnFrameInputTick(float frameDeltaTime) override;
+		void DrawInspectorImGui() override;
+		[[nodiscard]] TICK_GROUP GetTickGroup() const override;
+
+	private:
+		InputSystem* mInput = nullptr;
+		ConsoleSystem* mConsole = nullptr;
+
+		ConVar<float>* mTimeScale = nullptr;
+
+		uint64_t mSettingsCanvasGuid = 0u;
+		uint64_t mPauseCanvasGuid    = 0u;
+
+		bool mIsPaused = false;
+	};
+}
